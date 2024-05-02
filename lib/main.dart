f546894dc7f89cf79b2fd309a42a900e8a2daa2f
Amira Tarek <amirat737@gmail.com>
2024-05-02 15:11:29 +0300

    Fix providing display note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/cubits/display_note/display_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/utils/constants.dart';
 import 'package:notes_app/utils/simple_bloc_observer.dart';
@@ -9,7 +10,7 @@ import 'package:notes_app/views/home_view.dart';
 void main() async {
   await Hive.initFlutter();
   Bloc.observer = SimpleBlocObserver();
-    Hive.registerAdapter(NoteModelAdapter());
+  Hive.registerAdapter(NoteModelAdapter());
 
   await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const NotesApp());
@@ -22,15 +23,18 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Gilroy-Regular',
-        colorScheme: const ColorScheme.dark(
-          background: black,
+    return BlocProvider(
+      create: (context) => DisplayNoteCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Gilroy-Regular',
+          colorScheme: const ColorScheme.dark(
+            background: black,
+          ),
         ),
+        home: const HomeView(),
       ),
-      home: const HomeView(),
     );
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,30 +5,39 @@ import 'package:notes_app/widgets/floating_button_widget.dart';
 import 'package:notes_app/widgets/note_list_widget.dart';
 import 'package:notes_app/widgets/search_widget.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  @override
+  void initState() {
+    BlocProvider.of<DisplayNoteCubit>(context).displayNote();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => DisplayNoteCubit(),
-      child: const Scaffold(
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 25,
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              SearchWidget(),
-              NoteListWidget(),
-            ],
-          ),
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 25,
         ),
-        floatingActionButton: FloatingButtonWidget(),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            SearchWidget(),
+            NoteListWidget(),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingButtonWidget(),
     );
   }
 }
