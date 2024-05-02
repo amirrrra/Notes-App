e6d17105f82b3ea80ffdd2a4146f132a97800493
Amira Tarek <amirat737@gmail.com>
2024-05-02 11:27:27 +0300

    Add displayNote method to display note cubit

--- lib/cubits/display_note/display_note_cubit.dart
+++ lib/cubits/display_note/display_note_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/utils/constants.dart';
 
 part 'display_note_state.dart';
 
 class DisplayNoteCubit extends Cubit<DisplayNoteState> {
   DisplayNoteCubit() : super(DisplayNoteInitial());
+  displayNote() {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      // Iterable<E> => Collection of E
+      List<NoteModel> notesList = notesBox.values.toList();
+      emit(DisplayNoteSuccess(notesList: notesList));
+    } catch (e) {
+      emit(DisplayNoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
