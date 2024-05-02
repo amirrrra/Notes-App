01f79a2977327dea3b2ca8bf8f16c00497f2eedd
Amira Tarek <amirat737@gmail.com>
2024-05-02 11:27:52 +0300

    Provide display note cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/display_note/display_note_cubit.dart';
 import 'package:notes_app/widgets/floating_button_widget.dart';
 import 'package:notes_app/widgets/note_list_widget.dart';
 import 'package:notes_app/widgets/search_widget.dart';
@@ -8,24 +10,25 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body:  Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 25,
+    return BlocProvider(
+      create: (context) => DisplayNoteCubit(),
+      child: const Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 25,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              SearchWidget(),
+              NoteListWidget(),
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            SearchWidget(),
-            NoteListWidget(),
-          ],
-        ),
+        floatingActionButton: FloatingButtonWidget(),
       ),
-      floatingActionButton: FloatingButtonWidget(),
     );
   }
 }
-
-
