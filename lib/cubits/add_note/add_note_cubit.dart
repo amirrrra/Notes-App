aabfd64f981742f1487dd47ac3cf275b1b7f6e07
Amira Tarek <amirat737@gmail.com>
2024-05-03 05:13:36 +0300

    Trigger picking a color

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:bloc/bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
@@ -9,7 +11,10 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color? color = babyBlue;
   addNote(NoteModel noteModel) async {
+    noteModel.color = color!.value;
+
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
--- lib/widgets/color_list_widget.dart
+++ lib/widgets/color_list_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note/add_note_cubit.dart';
 import 'package:notes_app/utils/constants.dart';
 import 'package:notes_app/widgets/color_widget.dart';
 
@@ -10,6 +12,7 @@ class ColorListWidget extends StatefulWidget {
   @override
   State<ColorListWidget> createState() => _ColorListWidgetState();
 }
+
 class _ColorListWidgetState extends State<ColorListWidget> {
   int currentIndex = 0;
 
@@ -26,6 +29,7 @@ class _ColorListWidgetState extends State<ColorListWidget> {
             isActive: currentIndex == index,
             onTap: () {
               currentIndex = index;
+              BlocProvider.of<AddNoteCubit>(context).color = colorsList[index];
               setState(() {});
             },
           );
